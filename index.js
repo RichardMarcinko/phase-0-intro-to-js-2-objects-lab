cf31e792a1d680c500550831a7ba6ca455f6e6bc
John Coleman <jbcolemanjr@gmail.com>
2023-08-03 11:31:27 -0400

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Roger Waters",
+    streetAddress: "237 Washer Avenue",
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+
+const luisObject = {
+    name: "Luis",
+    favoriteMovie: "Star Wars",
+}
+
+
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(luisObject, "favoriteMovie", "Shrek"))
+console.log(luisObject)
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key]
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
